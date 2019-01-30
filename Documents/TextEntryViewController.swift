48bd42699bfacc8377f384d715a0039e37977c67
Weston Verhulst <w2visme@gmail.com>
2019-01-29 18:56:12 -0600

    worked on storyboard

---
+++ Documents/TextEntryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TextEntryViewController.swift
+//  Documents
+//
+//  Created by Weston Verhulst on 1/29/19.
+//  Copyright © 2019 Weston Verhulst. All rights reserved.
+//
+
+import UIKit
+
+class TextEntryViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func saveButton(_ sender: Any) {
+        self.title = nameTextField.text
+    }
+    
+}
